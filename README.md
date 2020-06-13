d34dffbc0107170b437741acc26f47520aeb4377
mazilkhatib <mazilkhatib786@gmail.com>
2020-06-13 21:59:29 +0530

    chore door

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1 align="center">Welcome to Chore Door! 👋</h1>
+<p>
+</p>
+
+> A javaScript game to demonstrate DOM functionalities.
+
+## Author
+
+👤 **Mazil Khatib**
+
+* Website: https://mazilkhatib.github.io/resume
+* Github: [@mazilkhatib](https://github.com/mazilkhatib)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ script.js
@@ -0,0 +1,115 @@
+var doorImage1 = document.getElementById('door1')
+var doorImage2 = document.getElementById('door2')
+var doorImage3 = document.getElementById('door3')
+var botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg'
+
+let currentlyPlaying = true;
+
+let closedDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg'
+
+let beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg'
+
+let spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg'
+
+let numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+let startButton = document.getElementById('start')
+
+const isBot = (door) =>{
+  if(door.src === botDoorPath){
+    return true
+  }
+  else{
+    return false;
+  }
+}
+
+const isClicked = (door) =>{
+  if (door.src === closedDoorPath) {
+  return false;
+} else {
+  return true;
+}
+}
+
+const playDoor = (door) => {
+    numClosedDoors--;
+    if (numClosedDoors === 0) {
+        gameOver('win');
+    }
+    else if (isBot(door) === true) {
+        return gameOver();
+    }
+}
+const randomChoreDoorGenerator = () =>{
+   const choreDoor = Math.floor(Math.random() * numClosedDoors);
+
+  if(choreDoor === 0){
+    openDoor1 = botDoorPath;
+    openDoor2 = beachDoorPath;
+    openDoor3 = spaceDoorPath;
+
+  }  else if(choreDoor === 1){
+    openDoor2 = botDoorPath;
+    openDoor1 = beachDoorPath;
+    openDoor3 = spaceDoorPath;
+
+  } else if(choreDoor === 2){
+    openDoor3 = botDoorPath;
+    openDoor2 = spaceDoorPath;
+    openDoor1 = beachDoorPath;
+  }
+}
+
+doorImage1.onclick = () => {
+  if(!isClicked(doorImage1) && currentlyPlaying) {
+ doorImage1.src = openDoor1;
+  playDoor(doorImage1);
+}
+
+}
+doorImage2.onclick = () => {
+  if(!isClicked(doorImage2) && currentlyPlaying) {
+doorImage2.src = openDoor2;
+  playDoor(doorImage2);
+}
+
+}
+doorImage3.onclick = () => {
+  if(!isClicked(doorImage3) && currentlyPlaying) {
+  doorImage3.src = openDoor3;
+  playDoor(doorImage3);
+}
+
+}
+
+let startRound = () => {
+  numClosedDoors = 3;
+  doorImage1.src = closedDoorPath;
+  doorImage2.src = closedDoorPath;
+  doorImage3.src = closedDoorPath;
+  startButton.innerHTML = 'Good luck!'
+  currentlyPlaying = true;
+  randomChoreDoorGenerator();
+}
+
+startButton.onclick = () => {
+    if (!currentlyPlaying) {
+        startRound();
+    }
+}
+
+
+const gameOver = (status) => {
+    if (status === 'win') {
+        startButton.innerHTML = "You win! Play again?";
+    } else {
+        startButton.innerHTML = "Game over! Play again?"
+    }
+    currentlyPlaying = false;
+}
+
+startRound();
